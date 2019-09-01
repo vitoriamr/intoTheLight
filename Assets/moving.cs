a4dfea2229d9bcddc8a870e2313196beae1171c7
Rafael Petersen <rafapetersenpet@gmail.com>
2019-09-01 19:59:51 -0300

    Fist Version

---
+++ Assets/moving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    private GameObject lastSky;
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        lastSky = GameObject.FindWithTag("Finish");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!lastSky.GetComponent<Renderer>().isVisible) {
+            transform.Translate(0, Time.deltaTime, 0, Space.World);
+        }
+    }
+}
